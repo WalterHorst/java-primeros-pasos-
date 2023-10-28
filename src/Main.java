539935c81de79cddfe48169f9d1ad82ed7d04f61
WalterHorst <whorst42@gmail.com>
2023-10-28 20:18:25 -0300

    Variables

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo!");
+        String saludar = "Hola Mundo!";
+        int numerico = 10; //int es primitivo
+        System.out.println(saludar);
+        System.out.println("numero = " + numerico);
+
+
+        String nombre;
+
+        nombre = "Walter";
+
+if(numerico > 10){nombre = "Andres";}
+
+
     }
+
+
+    Integer numero = 10; //integer es un objeto y viene con metodos que puedo aplicar
+
+
+
 }
